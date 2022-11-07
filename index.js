aaa73c69ea03b6fbe64c4a7ef3057ae045d862fd
Larson Collier <collierlarson@gmail.com>
2022-11-07 02:39:43 -0500

    commit tree 930411d5e2350be038e32db5ef93be616db8471b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+    let userForm = document.getElementById("user-form")
+    userForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        
+        let req = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json',
+                        "Accept": "application/json"},
+            body: JSON.stringify({email: `${e.target.email.value}`, password: `${e.target.name.value}`})
+        })
+        
+    })
+
+fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    });
